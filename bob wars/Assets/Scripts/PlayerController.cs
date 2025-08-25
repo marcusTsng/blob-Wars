fbbc6e9f0da53bc9e8ccfcebd93de8087a5a2c40
Marcus Tsang <marcuskmTsang@gmail.com>
2025-08-25 13:34:05 +0800

    finished movement and camera

--- bob wars/Assets/Scripts/PlayerController.cs
+++ bob wars/Assets/Scripts/PlayerController.cs
@@ -17,26 +17,38 @@ public class PlayerController : MonoBehaviour
 
     // Inputs
     private float _inpX, _inpY = 0f;
-    private float _camX = 0f;
-    private float _camY = 0f;
+    private float _camX, _camY = 0f;
 
     private void Awake(){
-
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Start(){
-
     }
 
     private void Update(){
-        _camTransform.rotation = Quaternion.Euler(new Vector3(_camY * _camSensitivity, 0, 0));
+        // CAMERA CONTROLS
+        _camTransform.localRotation = Quaternion.Euler(new Vector3(_camY * _camSensitivity, 0, 0));
         transform.rotation = Quaternion.Euler(new Vector3(0, _camX * _camSensitivity, 0));
 
         _camX += Input.GetAxisRaw("Mouse X");
         _camY -= Input.GetAxisRaw("Mouse Y");
+
+        // MOVEMENT
+        if (Input.GetKey(KeyCode.W)){
+            _rb.AddForce(transform.forward * _moveSpeed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            _rb.AddForce(transform.right * _moveSpeed * -1, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            _rb.AddForce(transform.forward * _moveSpeed * -1, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            _rb.AddForce(transform.right * _moveSpeed, ForceMode.Impulse);
+        }
     }
 
     private void FixedUpdate(){
-
     }
 }
