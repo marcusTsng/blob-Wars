1a5e046b9563dce62199e1addcd8ef7882168a8e
Marcus Tsang <marcuskmTsang@gmail.com>
2025-08-26 17:14:04 +0800

    added movement system with jumping
    movement added, but its kinda yucky so we migght need to change some stuff

--- bob wars/Assets/Scripts/PlayerController.cs
+++ bob wars/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(Transform))]
 public class PlayerController : MonoBehaviour
-{   
+{
     [Header("References")]
     private Rigidbody _rb;
     [SerializeField] private Transform _camTransform;
-    
+
     [Header("Player Stats")]
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpPower;
 
     [Header("Camera Stats")]
     [SerializeField] private float _camSensitivity;
@@ -19,14 +20,22 @@ public class PlayerController : MonoBehaviour
     private float _inpX, _inpY = 0f;
     private float _camX, _camY = 0f;
 
-    private void Awake(){
+    // Check variables (for physics)
+    private bool _jump = false;
+    private bool _grounded = true;
+    private GameObject _groundedTo;
+
+    private void Awake()
+    {
         _rb = GetComponent<Rigidbody>();
     }
 
-    private void Start(){
+    private void Start()
+    {
     }
 
-    private void Update(){
+    private void Update()
+    {
         // CAMERA CONTROLS
         _camTransform.localRotation = Quaternion.Euler(new Vector3(_camY * _camSensitivity, 0, 0));
         transform.rotation = Quaternion.Euler(new Vector3(0, _camX * _camSensitivity, 0));
@@ -35,20 +44,58 @@ public class PlayerController : MonoBehaviour
         _camY -= Input.GetAxisRaw("Mouse Y");
 
         // MOVEMENT
-        if (Input.GetKey(KeyCode.W)){
+        if (Input.GetKey(KeyCode.W))
+        {
             _rb.AddForce(transform.forward * _moveSpeed, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.A)){
+        if (Input.GetKey(KeyCode.A))
+        {
             _rb.AddForce(transform.right * _moveSpeed * -1, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.S)){
+        if (Input.GetKey(KeyCode.S))
+        {
             _rb.AddForce(transform.forward * _moveSpeed * -1, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.D)){
+        if (Input.GetKey(KeyCode.D))
+        {
             _rb.AddForce(transform.right * _moveSpeed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _jump = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_jump)
+        {
+            _jump = false;
+            Jump();
         }
     }
 
-    private void FixedUpdate(){
+    // JUMP FUNCTIONS
+    private void Jump()
+    {
+        if (_grounded)
+        {
+            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        }
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.gameObject.CompareTag("Player"))
+        {
+            _grounded = true;
+            _groundedTo = collider.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject == _groundedTo)
+        {
+            _grounded = false;
+            _groundedTo = null;
+        }
     }
 }
